c7f60b9f2c22f30796e7fc73ff70cd7c6fa1550f
Jersline Rose <jerslinerose@gmail.com>
2025-01-11 00:49:11 +0530

    Add production build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/navbar';
 import Hero from './components/hero'
 import ParticleBackground from './components/Particle'
 import About from './components/About'
